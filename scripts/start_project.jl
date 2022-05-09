a58141b3da1b3522b3008392df75d73869bc85fe
G Jake Gebbie <ggebbie@whoi.edu>
2022-05-09 10:17:38 -0400

    notebook target: translate to src and test

---
+++ scripts/start_project.jl
@@ -0,0 +1,5 @@
+using Revise, PkgTemplates
+
+t = Template(; user="ggebbie", dir="~/projects", authors="G Jake Gebbie", julia=v"1.6", plugins=[ License(; name="MIT"), Git(; manifest=true, ssh=true), GitHubActions(; x86=false, extra_versions=["1.6","1.7","nightly"]), Codecov(), Documenter{GitHubActions}(), Develop(), ], )
+
+t("SpectraFS.jl")
