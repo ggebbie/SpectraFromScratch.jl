096d65463a52c21fd7edf876afd435ad1007c9c4
G Jake Gebbie <ggebbie@whoi.edu>
2022-11-18 11:42:39 -0500

    bugfix docs package name

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,13 @@
-using SpectraFS
+using SpectraFromScratch
 using Documenter
 
-DocMeta.setdocmeta!(SpectraFS, :DocTestSetup, :(using SpectraFS); recursive=true)
+DocMeta.setdocmeta!(SpectraFromScratch, :DocTestSetup, :(using SpectraFS); recursive=true)
 
 makedocs(;
-    modules=[SpectraFS],
+    modules=[SpectraFromScratch],
     authors="G Jake Gebbie",
     repo="https://github.com/ggebbie/SpectraFS.jl/blob/{commit}{path}#{line}",
-    sitename="SpectraFS.jl",
+    sitename="SpectraFromScratch.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://ggebbie.github.io/SpectraFS.jl",
@@ -19,6 +19,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/ggebbie/SpectraFS.jl",
+    repo="github.com/ggebbie/SpectraFromScratch.jl",
     devbranch="main",
 )
